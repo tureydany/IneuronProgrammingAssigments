e870fc4e4f8f4bdb9be7a8d2590a468110ce3847
tureydany <52342775+tureydany@users.noreply.github.com>
2022-07-13 17:39:43 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+#1.Write a Python program to print &quot;Hello Python&quot;?
+def sample():
+    print("Hello Python")
+
+#2. Write a Python program to do arithmetical operations addition and division.?
+def add(a,b):
+    c=a+b
+    print("sum of two nos:",c)
+
+
+def division(a,b):
+    c=a/b
+    print("division of two nos :",c)
+
+sample()
+add(3,4)
+division(4,2)
